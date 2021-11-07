78e726fb1498bc649aa96badd0beca223a4c1c31
admin <admin@semiformal.net>
2021-11-07 10:28:06 -0500

    dangling

--- user.py
+++ user.py
@@ -11,24 +11,23 @@ class User(UserMixin):
 
     @staticmethod
     def get(user_id):
-        db = get_db()
-        user = db.execute(
-            "SELECT * FROM user WHERE id = ?", (user_id,)
-        ).fetchone()
-        if not user:
-            return None
-
-        user = User(
-            id_=user[0], name=user[1], email=user[2], profile_pic=user[3]
-        )
+        db = get_db() # conn()
+        with db.cursor() as cursor:
+            user = cursor.execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+            if not user:
+                return None
+            user = User(
+                id_=user[0], name=user[1], email=user[2], profile_pic=user[3] )
         return user
 
     @staticmethod
     def create(id_, name, email, profile_pic):
         db = get_db()
-        db.execute(
-            "INSERT INTO user (id, name, email, profile_pic) "
-            "VALUES (?, ?, ?, ?)",
-            (id_, name, email, profile_pic),
-        )
+        with db.cursor() as cursor:
+            cursor.execute(
+                "INSERT INTO user (id, name, email, profile_pic) "
+                "VALUES (?, ?, ?, ?)",
+                (id_, name, email, profile_pic),
+            )
+        
         db.commit()
